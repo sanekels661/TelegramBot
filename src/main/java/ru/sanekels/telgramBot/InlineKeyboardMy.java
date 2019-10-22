52583582d48c92bc7984adf084d923b44f2c057b
Александр <elsakov982@inbox.ru>
2019-10-22 09:45:56 +0300

    доделал доп задание + исправил ошибки 4

--- src/main/java/ru/sanekels/telgramBot/InlineKeyboardMy.java
+++ src/main/java/ru/sanekels/telgramBot/InlineKeyboardMy.java
@@ -1,4 +1,5 @@
 package ru.sanekels.telgramBot;
+
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
@@ -52,14 +53,12 @@ public class InlineKeyboardMy extends BotCommand {
     @Override
     public void execute(AbsSender absSender, User user, Chat chat, String[] arguments) {
         SendMessage answer = new SendMessage();
-        StringBuilder messageTextBuilder;
         if (arguments != null && arguments.length > 0) {
-            messageTextBuilder = new StringBuilder("inline answer");
+            answer.setText("inline answer");
         } else {
-            messageTextBuilder = new StringBuilder("inline examples");
+            answer.setText("inline examples");
         }
         answer.setChatId(chat.getId().toString());
-        answer.setText(messageTextBuilder.toString());
         answer.setReplyMarkup(getInlineKeyboardMarkup(arguments));
         try {
             absSender.execute(answer);
